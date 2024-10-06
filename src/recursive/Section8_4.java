a2a61dd2dca650b2b0e968d12ef691d90e78d69e
김윤제 <forteas2003@hanmail.net>
2024-10-06 11:48:45 +0900

    refactor: Section8_4 피보나취 재귀 (메모이제이션) 리팩토링

--- src/recursive/Section8_4.java
+++ src/recursive/Section8_4.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class Section8_4 {
     public static int n = 0;
+    static int[] fibo;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
-        for (int i = 1; i <= n; i++) {
+        fibo = new int[n+1];
+
+        /*for (int i = 1; i <= n; i++) {
             System.out.print(recursive(i) + " ");
+        }*/
+        fibonachi(n);
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibo[i] + " ");
         }
     }
 
@@ -17,6 +24,15 @@ public class Section8_4 {
         else if (n == 2) return 1;
         else {
             return recursive(n -2 ) + recursive(n - 1);
+        }
+    }
+
+    public static int fibonachi(int n) {
+        if(fibo[n] > 0) return fibo[n];
+        if (n == 1) return fibo[1] = 1;
+        else if (n == 2) return fibo[2] = 1;
+        else {
+            return fibo[n] = fibonachi(n -2 ) + fibonachi(n - 1);
         }
     }
 }
