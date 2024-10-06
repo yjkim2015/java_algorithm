288ae0b67dfe264c87eb7a51413462cdd71b9d4e
김윤제 <forteas2003@hanmail.net>
2024-10-06 12:29:19 +0900

    refactor: Section8_2 이진수 출력 (재귀)

--- src/recursive/Section8_2.java
+++ src/recursive/Section8_2.java
@@ -11,6 +11,9 @@ public class Section8_2 {
         recursive(n);
     }
     public static void recursive(int n) {
+        if (n == 0) return;
 
+        recursive(n / 2);
+        System.out.println(n % 2 + " ");
     }
 }
